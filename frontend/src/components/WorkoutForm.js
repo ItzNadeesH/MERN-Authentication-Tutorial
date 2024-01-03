b06c43d1f5ccec8bd4fa45ea72327ab28d2a712d
NadeesH <spnadeeshnimsara@gmail.com>
2024-01-03 13:43:52 +0530

    Initial commit

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { useWorkoutContext } from '../hooks/useWorkoutContext';
+
+const WorkoutForm = () => {
+  const { dispatch } = useWorkoutContext();
+  const [title, setTitle] = useState('');
+  const [reps, setReps] = useState('');
+  const [load, setLoad] = useState('');
+  const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const workout = { title, reps, load };
+
+    const response = await fetch('/api/workouts', {
+      method: 'POST',
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.msg);
+      setEmptyFields(json.msg);
+    }
+    if (response.ok) {
+      setTitle('');
+      setReps('');
+      setLoad('');
+      setError(null);
+      setEmptyFields([]);
+      console.log('new workout added', json);
+      dispatch({ type: 'CREATE_WORKOUT', payload: json });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="create">
+      <h3>Add a New Workout</h3>
+      <label>Exercise Title: </label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className={emptyFields.includes('title') ? 'error' : ''}
+      />
+      <label>Reps: </label>
+      <input
+        type="number"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+      />
+      <label>Load (in kg): </label>
+      <input
+        type="number"
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+      />
+      <button type="submit">Add Workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
+
+export default WorkoutForm;
